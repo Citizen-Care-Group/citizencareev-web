bf2bfb2a0e4739116f18a8af4de37629b0780a37
avianurag721 <anurag.721721@gmail.com>
2024-07-15 09:29:03 +0530

    urban mobility

--- src/Components/Home/Index.jsx
+++ src/Components/Home/Index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainCarousal from './MainCarousal'
 import ToggleEVSection2 from './ToggleEVSection2'
+import UrbanMobilityPosters from './UrbanMobilityPosters'
 
 const Index = () => {
   return (
-    <div>
+    <div className=' flex flex-col justify-center'>
       <MainCarousal />
-      <ToggleEVSection2/>
+      <ToggleEVSection2 />
+      <UrbanMobilityPosters/>
     </div>
   )
 }
--- src/Components/Home/UrbanMobilityPosters.jsx
+++ src/Components/Home/UrbanMobilityPosters.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import scooty from "../../Images/Scooty/scooty.png";
 
 const UrbanMobilityPosters = () => {
   return (
-    <div className=" flex flex-col">
+    <div className=" flex flex-col w-[90%] justify-center ">
       <div className=" flex ">
-        <div className=" w-[70%] "></div>
+        <div className=" w-[70%] flex bg-slate-700 ">
+          <ul className=" list-disc">
+            <li>Alloy Wheel Rims</li>
+            <li>190 MM Ground Clearance</li>
+            <li>Auto Repair Function</li>
+          </ul>
+          <img src={scooty}  className="object-cover overflow-hidden h-auto w-1/2" alt="scooty" />
+        </div>
         <div className=" w-[30%] "></div>
       </div>
       <div className=" flex items-center">
