5f3b5ede72448bdf0d0d527da2943a3637750f49
avianurag721 <anurag.721721@gmail.com>
2024-07-26 21:00:07 +0530

    news section done

--- src/Components/Home/News.jsx
+++ src/Components/Home/News.jsx
@@ -1,38 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { apiConnector } from "../../Services/connector";
+import { endpoints } from "../../Services/apis";
+import testImage from "../../Images/EV-About.jpg"
+const { GET_NEWS } = endpoints;
 
 const News = () => {
-  const news = [
-    {
-      photo: "image",
-      headline: "this is the new",
-      moreInfo: "this news is regarding the scooter vehicle",
-    },
-    {
-      photo: "image",
-      headline: "this is the new",
-      moreInfo: "this news is regarding the scooter vehicle",
-    },
-    {
-      photo: "image",
-      headline: "this is the new",
-      moreInfo: "this news is regarding the scooter vehicle",
-    },
-    {
-      photo: "image",
-      headline: "this is the new",
-      moreInfo: "this news is regarding the scooter vehicle",
-    },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [News, SetNews] = useState();
+
+  useEffect(() => {
+    const getAllScooties = async () => {
+      try {
+        setLoading(true);
+        const response = await apiConnector("GET", GET_NEWS);
+        console.log("newws data", response?.data);
+        SetNews(response?.data?.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error getting scooties:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getAllScooties();
+  }, []);
+
+  // const news = [
+  //   {
+  //     photo: "image",
+  //     headline: "this is the new",
+  //     moreInfo: "this news is regarding the scooter vehicle",
+  //   },
+  //   {
+  //     photo: "image",
+  //     headline: "this is the new",
+  //     moreInfo: "this news is regarding the scooter vehicle",
+  //   },
+  //   {
+  //     photo: "image",
+  //     headline: "this is the new",
+  //     moreInfo: "this news is regarding the scooter vehicle",
+  //   },
+  //   {
+  //     photo: "image",
+  //     headline: "this is the new",
+  //     moreInfo: "this news is regarding the scooter vehicle",
+  //   },
+  // ];
+
+  if (loading) {
+    return null;
+  }
   return (
     <div className=" w-full  flex flex-col justify-center items-center">
       <h1 className=" text-2xl font-bold">Reports & Headline</h1>
       <div className=" w-[90%] flex flex-wrap gap-2 m-4 justify-around">
-        {news.map((item, index) => (
-          <div key={index} className="flex gap-2 w-[45%] border-2 border-black">
-            <img src="" alt="newsImage" />
+        {News.map((item, index) => (
+          <div key={index} className="flex gap-2 w-[45%] border-2  border-black">
+            <img src={testImage} alt="newsImage" width={70} />
             <div className=" flex flex-col gap-2 items-start">
-              <h1> {item.headline} </h1>
-              <p> {item.moreInfo} </p>
+              <h1> {item.title} </h1>
+              <p> {item.date} </p>
             </div>
           </div>
         ))}
